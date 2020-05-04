feb283b7588e19f9bc0c7cdee3a1bfb02a7959ba
Siwon Lee <cool1726@naver.com>
2020-05-04 21:45:53 +0900

    클래스 구조

---
+++ accountHW1.py
@@ -0,0 +1,28 @@
+class Account:
+    def __init__(self):
+        self.name = ""
+        self.accountNum = ""
+        self.accountBal = 0
+
+    def createAcc(self, account):
+        if(account) :
+            pass
+
+    def deposit(self, accNum, amount):
+        pass
+
+    def withdraw(self, accNum, amount):
+        pass
+
+    def readAcc(self, account):
+        pass
+
+    #사용자 정보 관리
+
+x = list()
+
+a = Account()
+a.deposit("1111", 1000)
+
+x.append(a)
+x.append(2)
